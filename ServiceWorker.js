79265f100fb6467b5d0c61719e840e042f17d16f
Muhammad Hammad Arshad <ihammadarshad@gmail.com>
2025-07-03 05:22:08 +0500

    New Update - 03-07-2025

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "XEX-XEX-Game-0.3.0";
 const contentToCache = [
-    "Build/Xex Crypto Game WebGL Build-V(0.3.0) 20-06-2025.loader.js",
-    "Build/Xex Crypto Game WebGL Build-V(0.3.0) 20-06-2025.framework.js",
-    "Build/Xex Crypto Game WebGL Build-V(0.3.0) 20-06-2025.data",
-    "Build/Xex Crypto Game WebGL Build-V(0.3.0) 20-06-2025.wasm",
+    "Build/Xex Crypto Full Version WebGL Build-V(0.3.0) 01-07-2025.loader.js",
+    "Build/Xex Crypto Full Version WebGL Build-V(0.3.0) 01-07-2025.framework.js",
+    "Build/Xex Crypto Full Version WebGL Build-V(0.3.0) 01-07-2025.data",
+    "Build/Xex Crypto Full Version WebGL Build-V(0.3.0) 01-07-2025.wasm",
     "TemplateData/style.css"
 
 ];
